ccf897288cefb26ab64080695605e834baa14b4c
Mohanraj_Nesh <mohanraj.d@neshinc.com>
2022-01-27 09:32:45 +0530

    First commit on javascript basics
    all the learning progress can be tracked here!

---
+++ script.js
@@ -0,0 +1,15 @@
+function myAlert()
+{
+    // window.alert("You clicked for alert");
+    alert("An alert without window object");
+
+}
+function myWrite()
+{
+    document.write("Written by document.write method of javascript");
+}
+
+function printPage()
+{
+    window.print();
+}
